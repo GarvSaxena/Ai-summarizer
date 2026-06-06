168d71b5f2a5f29c00345b2e12437e3952ee73b7
Garv Saxena <91243091+GarvSaxena@users.noreply.github.com>
2026-06-07 00:30:27 +0530

    Update background.js

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 // types of schrome storage - sync, local
 // local - stores inside the current session/browser/account
 // sync - across all session/accounts
+// runs when the user has not added the gemini api key
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.get(["geminiApiKey"], (result)=>{
         if(!result.geminiApiKey){
